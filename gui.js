4b4e22630973d333e243c21860c3d1e56e37d795
Stefania Druga <druga.stefania@gmail.com>
2018-01-25 17:09:52 -0500

    Build for f3d87686985691b856e9fb55be4aac7b9ef78e17

--- gui.js
+++ gui.js
@@ -1,6 +1,6 @@
 webpackJsonp([0],{
 
-/***/ 136:
+/***/ 137:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -8,33 +8,37 @@ webpackJsonp([0],{
 
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
-__webpack_require__(137);
+__webpack_require__(138);
 
 var _react = __webpack_require__(0);
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactDom = __webpack_require__(12);
+var _reactDom = __webpack_require__(13);
 
 var _reactDom2 = _interopRequireDefault(_reactDom);
 
-var _reactModal = __webpack_require__(15);
+var _reactModal = __webpack_require__(16);
 
 var _reactModal2 = _interopRequireDefault(_reactModal);
 
-var _appStateHoc = __webpack_require__(25);
+var _analytics = __webpack_require__(10);
+
+var _analytics2 = _interopRequireDefault(_analytics);
+
+var _appStateHoc = __webpack_require__(26);
 
 var _appStateHoc2 = _interopRequireDefault(_appStateHoc);
 
-var _gui = __webpack_require__(26);
+var _gui = __webpack_require__(27);
 
 var _gui2 = _interopRequireDefault(_gui);
 
-var _projectLoaderHoc = __webpack_require__(31);
+var _projectLoaderHoc = __webpack_require__(32);
 
 var _projectLoaderHoc2 = _interopRequireDefault(_projectLoaderHoc);
 
-var _index = __webpack_require__(478);
+var _index = __webpack_require__(481);
 
 var _index2 = _interopRequireDefault(_index);
 
@@ -46,6 +50,9 @@ if (false) {
         return true;
     };
 }
+
+// Register "base" page view
+_analytics2.default.pageview('/');
 
 var App = (0, _appStateHoc2.default)((0, _projectLoaderHoc2.default)(_gui2.default));
 
@@ -59,18 +66,18 @@ _reactDom2.default.render(_react2.default.createElement(App, null), appTarget);
 
 /***/ }),
 
-/***/ 137:
+/***/ 138:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
 
 
-__webpack_require__(138).polyfill();
+__webpack_require__(139).polyfill();
 
 
 /***/ }),
 
-/***/ 138:
+/***/ 139:
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -124,13 +131,13 @@ module.exports = {
 
 /***/ }),
 
-/***/ 478:
+/***/ 481:
 /***/ (function(module, exports, __webpack_require__) {
 
 // style-loader: Adds some css to the DOM by adding a <style> tag
 
 // load the styles
-var content = __webpack_require__(479);
+var content = __webpack_require__(482);
 if(typeof content === 'string') content = [[module.i, content, '']];
 // Prepare cssTransformation
 var transform;
@@ -156,7 +163,7 @@ if(false) {
 
 /***/ }),
 
-/***/ 479:
+/***/ 482:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(2)(false);
@@ -173,5 +180,5 @@ exports.locals = {
 
 /***/ })
 
-},[136]);
+},[137]);
 //# sourceMappingURL=gui.js.map
