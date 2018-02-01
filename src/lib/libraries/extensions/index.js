3bdfff0254a99d700365a1aed8a20a3d67648c75
Stefania Druga <druga.stefania@gmail.com>
2018-02-01 18:19:17 -0500

    new extensions

--- src/lib/libraries/extensions/index.js
+++ src/lib/libraries/extensions/index.js
@@ -4,13 +4,77 @@ import penImage from './pen.png';
 import wedoImage from './wedo.png';
 import jiboImage from './Jibo_extension.png';
 import alexaImage from './Alexa_extension.png';
-import tutorImage from './Tutor_extension.png';
+import cognimateImage from './Cognimate_extension.png';
 import hueImage from './Hue_extension.png';
 import speechImage from './speech.png';
 import videoImage from './video-sensing.png';
+import cozmoImage from './Cozmo_extension.png';
+import ergoImage from './Ergo_extension.png';
+import sentimentImage from './Sentiment_extension.png';
+import clarifaiImage from './Clarifai_extension.png';
 
 
 export default [
+    {
+        name: 'Cognimate',
+        extensionURL: 'cognimate',
+        iconURL: cognimateImage,
+        description: 'Your coding learning companion',
+        featured: true
+    },
+    {
+        name: 'Jibo',
+        extensionURL: 'jibo',
+        iconURL: jiboImage,
+        description: 'Control Jibo robot',
+        featured: true
+    },
+    {
+        name: 'Alexa',
+        extensionURL: 'alexa',
+        iconURL: alexaImage,
+        description: 'Teach Alexa new skills',
+        featured: true
+    },
+    {
+        name: 'Smart Lights',
+        extensionURL: 'hue',
+        iconURL: hueImage,
+        description: 'Control your home Hue Lights',
+        featured: true
+    },
+    {
+        name: 'Cozmo',
+        extensionURL: 'cozmo',
+        iconURL: cozmoImage,
+        description: 'Control Cozmo robot',
+        featured: true
+        // disabled: true
+    },
+    {
+        name: 'Sentiment Analysis',
+        extensionURL: 'sentiment',
+        iconURL: sentimentImage,
+        description: 'Detect sentiment of your messages',
+        featured: true
+        // disabled: true
+    },
+    {
+        name: 'ErgoJr Robot',
+        extensionURL: 'ergo',
+        iconURL: ergoImage,
+        description: 'Control ErgoJr robot',
+        featured: true
+        // disabled: true
+    },
+    {
+        name: 'Image classification',
+        extensionURL: 'clarifai',
+        iconURL: clarifaiImage,
+        description: 'Detect images from your camera',
+        featured: true
+        // disabled: true
+    },
     {
         name: 'Music',
         extensionURL: 'music',
@@ -31,57 +95,13 @@ export default [
         iconURL: wedoImage,
         description: 'Program with lego wedo.',
         featured: true
-    },
-    {
-        name: 'Jibo',
-        extensionURL: 'jibo',
-        iconURL: jiboImage,
-        description: 'Control Jibo robot.',
-        featured: true
-    },
-    {
-        name: 'Tutor',
-        extensionURL: 'tutor',
-        iconURL: tutorImage,
-        description: 'Control Tutor.',
-        featured: true
-    },
-    {
-        name: 'Alexa',
-        extensionURL: 'alexa',
-        iconURL: alexaImage,
-        description: 'Teach Alexa new skills.',
-        featured: true
-    },
-    {
-        name: 'Smart Lights',
-        extensionURL: 'hue',
-        iconURL: hueImage,
-        description: 'Control your home Hue Lights',
-        featured: true
-    }, 
-    {
-        name: 'Speech',
-        extensionURL: '',
-        iconURL: speechImage,
-        description: 'Talk to your projects.',
-        featured: true,
-        disabled: true
-    },
-    {
-        name: 'Video Sensing',
-        extensionURL: '',
-        iconURL: videoImage,
-        description: 'Detect motion with the camera.',
-        featured: true,
-        disabled: true
-    },
-    {
-        name: 'LEGO Boost',
-        extensionURL: '',
-        iconURL: boostImage,
-        description: 'Build robots and more.',
-        featured: true,
-        disabled: true
     }
+    // {
+    //     name: 'Wolfram',
+    //     extensionURL: 'clarifai',
+    //     iconURL: ergoImage,
+    //     description: 'Detect images from your camera',
+    //     featured: true,
+    //     disabled: true
+    // },
 ];
