f187474d1c6fcdaa8442e4c52a6b83e711acc1d9
Stefania Druga <druga.stefania@gmail.com>
2018-04-01 20:35:54 -0400

    update banners

--- src/lib/libraries/extensions/index.js
+++ src/lib/libraries/extensions/index.js
@@ -14,8 +14,10 @@ import sentimentImage from './Sentiment_extension.png';
 import clarifaiImage from './Clarifai_extension.png';
 import circuitImage from './CP_extension.png';
 import microbitImage from './Microbit_extension.png';
-import trackingImage from './Tracking_extension.png';
-import watsonImage from './Watson_extension.png';
+import trackingImage from './Video_sensing.png';
+import watsonVision from './Vision_extension.png';
+import watsonNlc from './Nlc_extension.png';
+
 // import speechImage from './Speech_extension.png';
 
 export default [
@@ -44,7 +46,7 @@ export default [
         name: 'Smart Lights',
         extensionURL: 'hue',
         iconURL: hueImage,
-        description: 'Control your home Hue Lights',
+        description: 'Control your home lights',
         featured: true
     },
     {
@@ -64,77 +66,77 @@ export default [
         // disabled: true
     },
     {
-        name: 'ErgoJr Robot',
+        name: 'Ergo Robot',
         extensionURL: 'ergo',
         iconURL: ergoImage,
-        description: 'Control ErgoJr robot',
+        description: 'Control Ergo robot',
         featured: true
         // disabled: true
     },
     {
-        name: 'Image classification',
+        name: 'Clarifai',
         extensionURL: 'clarifai',
         iconURL: clarifaiImage,
         description: 'Detect images from your camera',
         featured: true
         // disabled: true
     },
-    {
-        name: 'Circuit Playground',
-        extensionURL: 'circuit',
-        iconURL: circuitImage,
-        description: 'Change board LEDS and sounds',
-        featured: true
-        // disabled: true
-    },
-    {
-        name: 'Microbit',
-        extensionURL: 'microbit',
-        iconURL: microbitImage,
-        description: 'Detect gestures and program display',
-        featured: true
-        // disabled: true
-    },
+    // {
+    //     name: 'Circuit Playground',
+    //     extensionURL: 'circuit',
+    //     iconURL: circuitImage,
+    //     description: 'Change board LEDS and sounds',
+    //     featured: true
+    //     // disabled: true
+    // },
+    // {
+    //     name: 'Microbit',
+    //     extensionURL: 'microbit',
+    //     iconURL: microbitImage,
+    //     description: 'Detect gestures and program display',
+    //     featured: true
+    //     // disabled: true
+    // },
     {
         name: 'Music',
         extensionURL: 'music',
         iconURL: musicImage,
-        description: 'Play instruments and drums.',
+        description: 'Play instruments and drums',
         featured: true
     },
     {
-        name: 'Tracking',
+        name: 'Color',
         extensionURL: 'tracking',
         iconURL: trackingImage,
-        description: 'Color tracking.',
+        description: 'Color sensing',
         featured: true
     },
     {
         name: 'Pen',
         extensionURL: 'pen',
         iconURL: penImage,
-        description: 'Draw with your sprites.',
+        description: 'Draw with your sprites',
         featured: true
     },
     {
         name: 'Lego Wedo',
         extensionURL: 'wedo2',
         iconURL: wedoImage,
-        description: 'Program with lego wedo.',
+        description: 'Program with lego wedo',
         featured: true
     },
     {
-        name: 'Watson',
-        extensionURL: 'watson',
-        iconURL: watsonImage,
-        description: 'Machine learning with IBM Watson.',
+        name: 'Vision',
+        extensionURL: 'watson_vision',
+        iconURL: watsonVision,
+        description: 'Vision Training',
         featured: true
     },
     {
-        name: 'Watson Text Classifier',
+        name: 'Text',
         extensionURL: 'watson_nlc',
-        iconURL: watsonImage,
-        description: 'Machine learning with IBM Watson.',
+        iconURL: watsonNlc,
+        description: 'Text Training',
         featured: true
     },
     {
