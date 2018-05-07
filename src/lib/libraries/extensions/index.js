3745c9dd2a9a4187ccca9c3ba0fc59a82fb02683
satvu <sarahvu@mit.edu>
2018-05-07 13:57:31 -0400

    Revert "Revert "edit nav bar""
    This reverts commit 39fb17a64454bf63b7d152670ae4dcf68f4f65fc.

--- src/lib/libraries/extensions/index.js
+++ src/lib/libraries/extensions/index.js
@@ -122,7 +122,7 @@ export default [
     },
     {
         name: 'Speech',
-        extensionURL: 'watson_speech',
+        extensionURL: 'speech',
         iconURL: speechImage,
         description: 'Speech to text',
         featured: true
@@ -133,14 +133,14 @@ export default [
         iconURL: microbitImage,
         description: 'Program your Microbit',
         featured: true
-    },
+    },/*
     {
         name: 'Translation',
         extensionURL: 'translate',
         iconURL: translationImage,
         description: 'Translate between languages',
         featured: true
-    }, 
+    },*/ 
     {
         name: 'Lego Wedo',
         extensionURL: 'wedo2',
